12a30a9de07ea28d1f4f4adc17c62d7d45e55970
jorgedaw2022 <106011677+jorgedaw2022@users.noreply.github.com>
2022-05-22 11:21:01 +0200

    Add files via upload

---
+++ practica-rodriguez.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @author Jorge
+* @version 1.0
+*/
+
+/**
+*Función suma de dos números
+* @param int $num1
+* @param int $num2
+* @return float
+*/
+
+function suma($num1, $num2){
+$resultadosuma = $sum1 + $num2;
+return $resultadosuma;
+}
+
+$suma = suma (24,42);
+echo $suma;
+
+?>
